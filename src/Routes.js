1b775ef753787e13fb4e8c02e2aa7b3a8cf07c96
pedrofrxncx <pedro_vaz_franca@hotmail.com>
2022-12-17 11:32:00 -0300

    feat: Create navbar

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Posts from './pages/Posts'
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <>
       <Route exact path="/" component={Home} />
       <Route path="/posts" component={Posts} />
-    </BrowserRouter>
+    </>
   )
 }
